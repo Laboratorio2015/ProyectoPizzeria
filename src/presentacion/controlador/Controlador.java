239bf0b82a0ea81426c4eacbe2ff454626794ada
leandro123456 <leandrogguzman@hotmail.com>
2015-05-22 20:04:44 -0300

    cometee controlador new

--- src/presentacion/controlador/Controlador.java
+++ src/presentacion/controlador/Controlador.java
@@ -7,66 +7,124 @@ import java.util.List;
 
 import org.eclipse.swt.events.MouseEvent;
 
+import Monitor.Cocina.PadreMonitor;
+
 import modelo.Clientes;
+import modelo.Items;
 import modelo.Pedidos;
 import modelo.Productos;
 import dto.ClienteDTO;
+import dto.ItemDTO;
 import dto.PedidoDTO;
 import dto.ProductoDTO;
 import presentacion.vista.VentanaPrincipal;
 import presentacion.vista.ordenDePedido;
+import presentacion.vista.ordenarMatPrima;
 import presentacion.vista.pedidosPendientes;
+import presentacion.vista.registrarCobroDePedido;
+import presentacion.vista.registrarCobroManualmente;
+import presentacion.vista.registroDeCliente;
 import presentacion.vista.seleccionDeCliente;
+import presentacion.vista.seleccionarRepartidor;
 
-public class Controlador 
+public class Controlador implements ActionListener
 {
+	//ventanas
 	private VentanaPrincipal ventana;
 	private ordenDePedido ventanaPedido;
-	private seleccionDeCliente ventanaCliente;
 	private pedidosPendientes ventanaPedPendiente;
+	private seleccionDeCliente ventanaCliente;
+	private ordenarMatPrima ventanaMatPrima;
+	private registrarCobroDePedido ventanaRegCobroPedido;
+	private registrarCobroManualmente ventanaRegCobroManual;
+	private seleccionarRepartidor ventanaSeleccionRepartidor;
+	private registroDeCliente ventanaRegCliente;
+	private PadreMonitor monitorCocina;
 	
+	//dto
 	private List<PedidoDTO> listaPedidos;
 	private List<ProductoDTO> listaProductos;
 	private List<ClienteDTO> listaClientes;
-	private ArrayList<String> nombreProductos;
+	private List<ItemDTO> listaItems;
+	
+	//modelo
 	private Productos producto;
 	private Pedidos pedido;
 	private Clientes cliente;
+	private Items item;
 	
-	public Controlador(VentanaPrincipal ventana, Pedidos pedido, Clientes cliente) 
+	public Controlador(VentanaPrincipal ventana, Pedidos pedido, Clientes cliente,Productos producto, Items item) 
 	{
 		this.ventana=ventana;
 		this.pedido=pedido;
 		this.cliente=cliente;
-		this.listaPedidos=null;
-		this.listaProductos=null;
-		this.listaClientes=null;
+		this.item=item;
+		this.producto=producto;
+		
+		this.listaClientes=this.cliente.obtenerClientes();
+		this.listaProductos=this.producto.obtenerProducto();
+		this.listaItems=this.item.obtenerItems();
+		this.listaPedidos=this.pedido.obtenerPedidos();
+		
+		this.ventana.getBtnIngresarPedido().addActionListener(this);
+		this.ventana.getBtnPedidosPendientes().addActionListener(this);
+		
 	}
 	
 	public void inicializar()
 	{
+		this.monitorCocina=new PadreMonitor(pedido);
+		this.ventana.show();
 		//this.llenarListas();
 		//this.llenarTextField();
-		this.ventana.show();
 		//ventana.actualizarPedidoPend();
 	}
-	//@Override
-	public void actionPerformed(ActionEvent e)
+	@Override
+	public void actionPerformed(ActionEvent e) 
 	{
-		if(e.getSource() == this.ventanaCliente.getBtnSeleccionar())
-			this.ventana.actualizarPedidoPend();
+		
+		if(e.getSource() == this.ventana.getBtnIngresarPedido())
+		{
+			ventanaPedido=new ordenDePedido(ventana,this);
+			this.ventanaPedido.getBtnOrdenar().addActionListener(this);
+			this.ventanaPedido.setVisible(true);
+		}
+		else if(e.getSource() == this.ventana.getBtnPedidosPendientes())
+		{
+			ventanaPedPendiente=new pedidosPendientes(ventana, this);
+			ventanaPedPendiente.llenarTabla();
+			this.ventanaPedPendiente.setVisible(true);
+		}
+		else if(e.getSource()==this.ventanaPedido.getBtnOrdenar())
+		{
+			PedidoDTO nuevoPedido=new PedidoDTO();
+			nuevoPedido.setIdpedido(this.getListaPedidos().size()+1);
+			nuevoPedido.set_estado("solicitado");
+			nuevoPedido.setTotal(Integer.parseInt(this.ventanaPedido.getTfTotal().getText()));
+			nuevoPedido.set_comanda(nuevoPedido.getIdpedido());
+			nuevoPedido.set_ticket(nuevoPedido.getIdpedido());
+			nuevoPedido.setProductos(generarListaItems());
+			this.ventanaCliente=new seleccionDeCliente(this,nuevoPedido);
+			this.ventanaCliente.setVisible(true);
+			vaciarFormulario();
+			this.ventanaPedido.dispose();
+		}
 	}
-	
-	//@Override
-	//public void btnPedidosPendientes.addMouseListener(new MouseAdapter() 
-	//{
-	//@Override
-	/*public void mouseClicked(MouseEvent e) 
-	{
-		if(e.getSource()==this.ventana.getBtnPedidosPendientes())
-			this.ventanaPedPendiente=new pedidosPendientes(ventana,this);
-			//pedidosPendientes pedidoPendiente=new pedidosPendientes(_this);
-			ventanaPedPendiente.setVisible(true);
+
+	public List<PedidoDTO> getListaPedidos() {
+		return listaPedidos;
 	}
-	//});*/
-}
+
+	public void setListaPedidos(List<PedidoDTO> listaPedidos) {
+		this.listaPedidos = listaPedidos;
+	}
+
+	public List<ProductoDTO> getListaProductos() {
+		return listaProductos;
+	}
+
+	public void setListaProductos(List<ProductoDTO> listaProductos) {
+		this.listaProductos = listaProductos;
+	}
+
+
